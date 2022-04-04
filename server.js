b0a7fb2f18bb5c63828d4baf1bdd5cc6fde8953b
Yaroslav-Bonadarenko <y_bondarenko-itssv@kw.com>
2022-04-04 11:41:01 +0300

    My first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+
+const routes = require('./api/routes');
+routes(app);
+app.listen(port, ()=>{
+    console.log('Server started on port ' + port);
+})
